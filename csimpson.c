86b4d5266a462ee507a7e9605ced9260c6dc089c
Brad Safnuk <brad.safnuk@cmich.edu>
2011-03-09 15:09:11 -0500

    Completed Simpson's algorithm.

--- csimpson.c
+++ csimpson.c
@@ -1,21 +1,63 @@
 /* csimpson.c
  */
 
-double integrate(double (*f)(double ), double a, double b, double dx)
+#include <stdlib.h>
+#include <stdio.h>
+#include "csimpson.h"
+
+double g(double x, void *instance)
+{
+        return 4*x*x*x*x + 2/(x*x);
+}
+
+int main()
+{
+        double result;
+        double a, b, h;
+        a=2; b=5; h=0.1;
+        result = integrate(&g, a, b, h, NULL);
+        printf("Int(g, %f, %f) = %f  (dx=%f)\n", a, b, result, h);
+}
+
+
+/* Use Simpson's rule to estimate the integral of f over
+ * the interval [a, b]. The subdivision used is an even number
+ * of subintervals which have width <= dx.
+ *
+ * The instance variable points to user defined data for use
+ * by the callback function.
+ */
+double integrate(double (*f)(double, void *), 
+                double a, double b, double dx, void *instance)
 {
         int i;
-        int n = calculate_number_of_intervals(a, b, dx);
+        double h = dx;
+        int n = calculate_number_of_intervals(a, b, &h);
         double x = a; 
-        double sum = f(x);
+        double sum = f(x, instance);
         
+
         for (i=2; i<n; i+=2) {
-                x = a + i*dx;
-                sum += 2 * f(x);
+                x = a + i*h;
+                sum += 2 * f(x, instance);
         }
-        for (i=1; i<n i+=2) {
-                x = a + i * dx;
-                sum += 4 * f(x);
+        for (i=1; i<n; i+=2) {
+                x = a + i * h;
+                sum += 4 * f(x, instance);
         }
-        sum += f(b);
-        return sum;
+        sum += f(b, instance);
+        return sum * h / 3;
+}
+
+/* Returns the smallest integer n which is even and satisfies
+ *      |b-a| / n  <  dx
+ * Also resets dx to be the correct step size ( = |b-a|/n )
+ */
+int calculate_number_of_intervals(double a, double b, double *dx)
+{
+        double width = a < b ? (b-a) : (a-b); // width = |b-a|
+        int n = (int)(width / (*dx) + 1);
+        n =  n % 2 ? n+1 : n; // add 1 to n if it is odd
+        *dx = width / n;
+        return n;
 }
--- csimpson.h
+++ csimpson.h
@@ -1,5 +1,7 @@
 /* csimpson.h
  */
 
-double integrate(double (*f)(double ), double a, double b, double dx);
+double integrate(double (*f)(double, void *), 
+    double a, double b, double dx, void *instance);
+int calculate_number_of_intervals(double a, double b, double *dx);
 
